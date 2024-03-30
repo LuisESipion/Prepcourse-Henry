1173dfb2f4e39c0c63e2d536eb3be390f150eb11
Luis Sipion <luis.sipion017@gmail.com>
2024-03-29 23:45:49 -0500

    HMW5 progress 1

--- C5 Fundamentos Javascript/ejercicios/08.js
+++ C5 Fundamentos Javascript/ejercicios/08.js
@@ -3,6 +3,12 @@ function esNumeroEntero(numero) {
   // Verifica si este es un número entero o no.
   // Returna true si lo es, de lo contrario, retorna false.
   // Tu código:
+  var p_int = Math.floor(numero)
+  if (typeof numero != 'number') {
+    return false
+  } else {
+    return numero - p_int === 0
+  }
 }
 
 module.exports = esNumeroEntero;
